8306d34c00b595d608c427c5bd9b1d4c49aeb87f
Nguyen Minh Phu <minhphu25102005@gmail.com>
2026-03-06 13:39:59 +0700

    Add health check endpoint for UptimeRobot

--- src/main/java/com/example/demo/controller/HealthCheckController.java
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -1,3 +1,4 @@
+package com.example.demo.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
